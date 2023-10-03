cdf90ab3541136f7c3c73e95c244057c7bb4875c
Aykut Umut <aykutumt@gmail.com>
2023-10-03 21:39:58 +0100

    merhaba githup

--- src/main/java/deneme01.java
+++ src/main/java/deneme01.java
@@ -1,5 +1,7 @@
 public class deneme01 {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("hi java");
+        System.out.println("hi githup");
     }
 }
